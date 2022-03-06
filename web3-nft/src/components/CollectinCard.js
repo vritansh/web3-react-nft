306b969f274fe6d10ca9e55a6368a10b20ee8e01
vritansh <vritansh14@gmail.com>
2022-03-06 18:57:12 +0530

    Added collection card div.

--- web3-nft/src/components/CollectinCard.js
+++ web3-nft/src/components/CollectinCard.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import weth from '../assets/weth.png'
 
-const CollectinCard = () => {
+const CollectinCard = ({id, name, traits, image}) => {
   return (
         <div className="collectionCard">
-                
+            <img src={{image}} />
+            <div className='details'></div>
+            <div className='name'>
+                {name} <div className='id'> .#{id} </div>
+            </div> 
+            <div className='priceContainer'>
+                <img src ={weth} className='wethImage' alt=''/>
+                <div className='price'> { traits[0] ?.value }</div>
+            </div>
   
         </div>
   )
