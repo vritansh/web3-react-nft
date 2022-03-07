6e035804f7b7a13dc9525d5e878c895e7f800ffc
vritansh <vritansh14@gmail.com>
2022-03-07 22:56:32 +0530

    Update with basic card styles.

--- web3-nft/src/App.js
+++ web3-nft/src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import CollectinCard from './components/CollectinCard';
+import { useState,useEffect } from 'react';
+
+
 
 function App() {
+
+
+
   return( 
   <div className= "app">
   <Header />
+
+  <CollectinCard id={0} name={'Bandana Punk'} traits={[{'value': 7}]} image='https://fatcats.nftlabs.to/assets/images/fatcatxrpl-1.png'   />     
+
   </div>
   );
      
--- web3-nft/src/components/CollectinCard.js
+++ web3-nft/src/components/CollectinCard.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import weth from '../assets/weth.png'
+import './CollectionCard.css'
 
 const CollectinCard = ({id, name, traits, image}) => {
+    
+console.log(image);
   return (
+     
         <div className="collectionCard">
-            <img src={{image}} />
-            <div className='details'></div>
+            <img src={image} />
+            <div className='details'>
             <div className='name'>
                 {name} <div className='id'> .#{id} </div>
             </div> 
+            </div>
             <div className='priceContainer'>
                 <img src ={weth} className='wethImage' alt=''/>
-                <div className='price'> { traits[0] ?.value }</div>
+                 <div className='price'> { traits[0] ?.value }</div>
             </div>
   
         </div>
